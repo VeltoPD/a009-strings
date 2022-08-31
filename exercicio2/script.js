63ccf9efd304cd18c8a16d6a31a1531d5975c15d
VeltoPD <veltopd@gmail.com>
2022-08-31 17:49:25 -0300

    Entrega dos Exercícios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(minhaString.length)
+
+let minhaNovaString = (minhaString.trim())
+console.log(minhaNovaString)
+
+console.log(minhaNovaString.length)
+
+minhaNovaString = minhaNovaString.replace("________","sticioso")
+console.log(minhaNovaString)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul,\ncom os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+console.log(frase)
+
+const fraseNova = frase.replace("verde","rosa").replace("azul","laranja")
+
+console.log(`Frase nova: ${fraseNova}`)
+
+const fraseMaius = "Mas não deixe o gato sair".toUpperCase()
+console.log(fraseNova.replace("mas não deixe o gato sair",fraseMaius))
